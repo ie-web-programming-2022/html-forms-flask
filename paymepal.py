64ce13e42306b462d3ab6657c5dcfb6b56ef982b
Pepe García <pepe@pepegar.com>
2022-02-09 01:17:06 +0100

    prepare code for class

--- paymepal.py
+++ paymepal.py
@@ -1,26 +1,9 @@
-from flask import Flask, request, render_template
-from data import users, transactions
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.form["username"]
-    password = request.form["password"]
-
-    if username in users and users[username] == password:
-        user_transactions = transactions[username]
-        
-        return render_template(
-            "private.html",
-            user=username,
-            transactions=user_transactions
-        )
-    else:
-        return render_template("unauthorized.html"), 403
 
 app.run(debug=True)
